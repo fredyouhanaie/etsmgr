cb1c40630751bdd4fdac04bdf7c4fc4317077fff
Fred Youhanaie <fyrlang@anydata.co.uk>
2019-04-03 00:41:56 +0100

    initial skeleton modules

---
+++ src/etsmgr_sup.erl
@@ -0,0 +1,74 @@
+%%%-------------------------------------------------------------------
+%%% @author Fred Youhanaie <fyrlang@anydata.co.uk>
+%%% @copyright (C) 2019, Fred Youhanaie
+%%% @doc
+%%%
+%%% Supervisor for the `etsmgr' application.
+%%%
+%%% @end
+%%% Created :  1 Apr 2019 by Fred Youhanaie <fyrlang@anydata.co.uk>
+%%%-------------------------------------------------------------------
+-module(etsmgr_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%% @end
+%%--------------------------------------------------------------------
+-spec start_link() -> {ok, Pid :: pid()} |
+		      {error, {already_started, Pid :: pid()}} |
+		      {error, {shutdown, term()}} |
+		      {error, term()} |
+		      ignore.
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart intensity, and child
+%% specifications.
+%% @end
+%%--------------------------------------------------------------------
+-spec init(Args :: term()) ->
+		  {ok, {SupFlags :: supervisor:sup_flags(),
+			[ChildSpec :: supervisor:child_spec()]}} |
+		  ignore.
+init([]) ->
+
+    SupFlags = #{strategy => one_for_one,
+		 intensity => 1,
+		 period => 5},
+
+    Child1 = #{id => 'etsmgr_srv',
+	       start => {'etsmgr_srv', start_link, []},
+	       restart => permanent,
+	       shutdown => 5000,
+	       type => worker,
+	       modules => ['etsmgr_srv']},
+
+    {ok, {SupFlags, [Child1]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
