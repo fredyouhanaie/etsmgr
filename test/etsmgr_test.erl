3299dff9516de129b9ff91ac055c0a304e42c71a
Fred Youhanaie <fyrlang@anydata.co.uk>
2019-06-26 19:33:08 +0100

    tidied up the unit tests.

--- test/etsmgr_test.erl
+++ test/etsmgr_test.erl
@@ -13,58 +13,83 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+% helper function for the tests
 check_server(Server, Inst_name) ->
     Process_name = etsmgr:inst_to_name(Server, Inst_name),
     erlang:whereis(Process_name) =/= undefined.
 
+% test the utility function
 etsmgr_inst_name_test() ->
     ?assert(etsmgr_srv == etsmgr:inst_to_name(etsmgr_srv, etsmgr)),
     ?assert(etsmgr_srv_aaa == etsmgr:inst_to_name(etsmgr_srv, aaa)).
 
+% start/stop the unnamed instance
 etsmgr_start_stop_1_test() ->
+
     ok = etsmgr:start(),
     ?assert(lists:keymember(etsmgr, 1, application:which_applications())),
     ?assert(check_server(etsmgr_srv, etsmgr)),
     ?assert(check_server(etsmgr_sup, etsmgr)),
+
     ok = etsmgr:stop(),
     ?assertNot(lists:keymember(etsmgr, 1, application:which_applications())).
 
+% start/stop a named instance
 etsmgr_start_stop_2_test() ->
     Name = aaa,
+
     ok = etsmgr:start(Name),
     ?assert(lists:keymember(Name, 1, application:which_applications())),
     ?assert(check_server(etsmgr_srv, Name)),
     ?assert(check_server(etsmgr_sup, Name)),
+
     ok = etsmgr:stop(Name),
     ?assertNot(lists:keymember(Name, 1, application:which_applications())).
 
+% simple new_table test
 etsmgr_new_table_1_test() ->
     ok = application:ensure_started(etsmgr),
+
     {ok, Mgr_pid, Table_id} = etsmgr:new_table(table1, ets_1, []),
     ?assert(self() == ets:info(Table_id, owner)),
     ?assert(Mgr_pid == ets:info(Table_id, heir)),
+
     {ok, Mgr_pid} = etsmgr:del_table(table1),
+    ets:delete(Table_id),
+
     ok = etsmgr:stop().
 
+% simple add_table test
 etsmgr_add_table_1_test() ->
     ok = application:ensure_started(etsmgr),
+
     ETS_id = ets:new(ets_1, []),
     {ok, Mgr_pid, Table_id} = etsmgr:add_table(table1, ETS_id),
     ?assert(self() == ets:info(Table_id, owner)),
     ?assert(Mgr_pid == ets:info(Table_id, heir)),
+
     {ok, Mgr_pid} = etsmgr:del_table(table1),
+    ets:delete(Table_id),
+
     ok = etsmgr:stop().
 
+% simple create/delete test
 etsmgr_del_table_1_test() ->
     ok = application:ensure_started(etsmgr),
+
     ETS_id = ets:new(ets_1, []),
     {ok, Mgr_pid, Table_id} = etsmgr:add_table(table1, ETS_id),
     ?assert(self() == ets:info(Table_id, owner)),
     ?assert(Mgr_pid == ets:info(Table_id, heir)),
+
     {ok, Mgr_pid} = etsmgr:del_table(table1),
+    ets:delete(Table_id),
+
     ok = etsmgr:stop().
 
+% deleting a non-existant table should return error
 etsmgr_del_table_2_test() ->
     ok = application:ensure_started(etsmgr),
+
     {error, no_such_table_entry} = etsmgr:del_table(table1),
     ok = etsmgr:stop().
