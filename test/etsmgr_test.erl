b3cc4315e07fd532aafab0ca97ef1053af674d97
Fred Youhanaie <fyrlang@anydata.co.uk>
2021-07-15 17:46:09 +0100

    replace delete table test with generator

--- test/etsmgr_test.erl
+++ test/etsmgr_test.erl
@@ -136,58 +136,62 @@ add_table_test_() ->
 
 %%-------------------------------------------------------------------
 
-% simple create/delete test
-etsmgr_del_table_1_test() ->
-    ok = application:ensure_started(etsmgr),
-
+del_table_1() ->
     ETS_id = ets:new(ets_1, []),
     {ok, Mgr_pid, Table_id} = etsmgr:add_table(table1, ETS_id),
-    ?assert(self() == ets:info(Table_id, owner)),
-    ?assert(Mgr_pid == ets:info(Table_id, heir)),
 
-    {ok, Mgr_pid} = etsmgr:del_table(table1),
-    ets:delete(Table_id),
+    ?assertEqual(self(), ets:info(Table_id, owner)),
+    ?assertEqual(Mgr_pid, ets:info(Table_id, heir)),
+    ?assertEqual({ok, Mgr_pid}, etsmgr:del_table(table1)),
+    ?assert(ets:delete(Table_id)),
+    true.
 
-    ok = etsmgr:stop().
-
-% deleting a non-existant table should return error
-etsmgr_del_table_2_test() ->
-    ok = application:ensure_started(etsmgr),
-
-    {error, no_such_table_entry} = etsmgr:del_table(table1),
-
-    ok = etsmgr:stop().
+del_table_2() ->
+    ?assertEqual({error, no_such_table_entry}, etsmgr:del_table(table1)),
+    true.
 
 % when a client has two, or more, tables under management, and we
 % delete one, we should still be linked to the table manager.
-etsmgr_del_table_3_test() ->
-    ok = application:ensure_started(etsmgr),
-
+del_table_3() ->
     % create/manage the first table
     {ok, Mgr_pid, Table_id1} = etsmgr:new_table(table1, ets_1, []),
-    ?assert(self() == ets:info(Table_id1, owner)),
-    ?assert(Mgr_pid == ets:info(Table_id1, heir)),
+
+    ?assertEqual(self(), ets:info(Table_id1, owner)),
+    ?assertEqual(Mgr_pid, ets:info(Table_id1, heir)),
+
     {links, Linked_pids1} = erlang:process_info(self(), links),
     ?assert(lists:member(Mgr_pid, Linked_pids1)),
 
     % create/manage the second table
     {ok, Mgr_pid, Table_id2} = etsmgr:new_table(table2, ets_2, []),
-    ?assert(self() == ets:info(Table_id2, owner)),
-    ?assert(Mgr_pid == ets:info(Table_id2, heir)),
+    ?assertEqual(self(), ets:info(Table_id2, owner)),
+    ?assertEqual(Mgr_pid, ets:info(Table_id2, heir)),
+
     {links, Linked_pids2} = erlang:process_info(self(), links),
     ?assert(lists:member(Mgr_pid, Linked_pids2)),
 
     % delete/unmanage the first table
-    {ok, Mgr_pid} = etsmgr:del_table(table1),
-    ets:delete(Table_id1),
+    ?assertEqual({ok, Mgr_pid}, etsmgr:del_table(table1)),
+    ?assert(ets:delete(Table_id1)),
 
     % we should still be linked to the manager pid
     {links, Linked_pids3} = erlang:process_info(self(), links),
     ?assert(lists:member(Mgr_pid, Linked_pids3)),
 
-    {ok, Mgr_pid} = etsmgr:del_table(table2),
-    ets:delete(Table_id2),
+    ?assertEqual({ok, Mgr_pid}, etsmgr:del_table(table2)),
+    ?assert(ets:delete(Table_id2)),
+    true.
 
-    ok = etsmgr:stop().
+del_table_test_() ->
+
+    {"delete table",
+     [{setup, fun setup/0, fun cleanup/1,
+       {inorder,
+        [ {"simple create/delete", ?_assert(del_table_1())},
+          {"deleting a non-existant table", ?_assert(del_table_2())},
+          {"deleting one of multiple table", ?_assert(del_table_3())}
+        ]}
+      }]
+    }.
 
 %%-------------------------------------------------------------------
